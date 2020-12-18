8d75dc8071fde283977f5b900cdaa4207c945c49
Artur-Pastana <reitutu_15@hotmail.com>
2020-12-18 16:43:32 -0300

    Pagamento, associação um-para-um

--- src/main/java/com/estudojava/curso/config/TestConfig.java
+++ src/main/java/com/estudojava/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudojava.curso.entities.Category;
 import com.estudojava.curso.entities.Order;
 import com.estudojava.curso.entities.OrderItem;
+import com.estudojava.curso.entities.Payment;
 import com.estudojava.curso.entities.Product;
 import com.estudojava.curso.entities.User;
 import com.estudojava.curso.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPreco());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
