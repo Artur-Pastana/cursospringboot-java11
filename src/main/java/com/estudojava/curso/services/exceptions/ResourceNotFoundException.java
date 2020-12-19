aba4127c58d576a4b91610576dac9fc22302bf12
Artur-Pastana <reitutu_15@hotmail.com>
2020-12-18 21:12:51 -0300

    Tratamento de exceções - excluir

--- src/main/java/com/estudojava/curso/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/estudojava/curso/services/exceptions/ResourceNotFoundException.java
@@ -5,7 +5,7 @@ public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	public ResourceNotFoundException(Object id) {
-		super("Recurso não encontrado Id " + id);
+		super("Recurso não encontrado com Id " + id);
 	}
 	
 	
